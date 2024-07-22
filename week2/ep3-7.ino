5108d18c955684541ae3e4161948585255cf17b8
Nnch <150988991+Natthawut-Ch01@users.noreply.github.com>
2024-07-22 16:12:17 +0700

    Create ep3-7.ino

---
+++ week2/ep3-7.ino
@@ -0,0 +1,52 @@
+#include "Arduino_LED_Matrix.h"
+ArduinoLEDMatrix matrix;
+
+void setup(){
+  matrix.begin();
+}
+
+uint8_t frame[8][12]= {
+  {0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0}
+};
+
+
+void loop(){
+  le();
+  re();
+  mnt();
+  ns();
+  matrix.renderBitmap(frame, 8, 12);
+}
+void le(){
+  frame[1][3]=1;
+  frame[1][4]=1;
+  frame[2][3]=1;
+  frame[2][4]=1;
+}
+void re(){
+  frame[1][8]=1;
+  frame[1][9]=1;
+  frame[2][8]=1;
+  frame[2][9]=1;
+}
+void mnt(){
+  frame[5][3]=1;
+  frame[5][9]=1;
+  frame[6][3]=1;
+  frame[6][4]=1;
+  frame[6][5]=1;
+  frame[6][6]=1;
+  frame[6][7]=1;
+  frame[6][8]=1;
+  frame[6][9]=1;
+}
+void ns(){
+  frame[3][5]=1;
+  frame[3][6]=1;
+}
