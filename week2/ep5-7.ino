e40753eb3d5e5ec84e0e30cf58d10d5155b10d20
Nnch <150988991+Natthawut-Ch01@users.noreply.github.com>
2024-07-22 16:36:24 +0700

    Update and rename ep5-7 to ep5-7.ino

---
+++ week2/ep5-7.ino
@@ -0,0 +1,19 @@
+#include "Arduino_LED_Matrix.h"   
+#include "animation.h"            
+ArduinoLEDMatrix matrix; 
+void setup() {
+  pinMode(13, OUTPUT);
+  matrix.loadSequence(animation);
+  matrix.begin();
+  matrix.play(true);
+}
+
+void loop() {
+  int d_time = 1000;
+  digitalWrite(13, 1);
+  delay(d_time);
+  digitalWrite(13, 0);
+  delay(d_time);
+
+}
+
